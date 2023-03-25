aa1855ee51873624664238b183d7954a8550e392
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-25 14:38:56 +0900

    Initial Commit

---
+++ ErrorHandling/ErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHandlingApp.swift
+//  ErrorHandling
+//
+//  Created by 渡邊魁優 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
